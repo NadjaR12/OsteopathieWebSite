06f3a50e5574f12e435979b5bda0d4e5a83b0981
Lorelai <nadja.rathenow@rub.de>
2023-03-11 11:05:37 +0100

    hover effect for cards - not the fional solution

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,18 +3,17 @@ import { useTranslation } from 'react-i18next';
 
 import './styles.scss';
 
-export default function Card({img, alt, title, description,text}) {
+export default function Card({title, description,text, className}) {
     const { t } = useTranslation();
 
     return (
-    <div>
-    <img src={img} alt={alt}/>
-    <h2>{t(title)}</h2>
-    <div>
-        <h3>{t(description)}</h3>
-        <h3>{t('AboutOsteo.categories.applied')}</h3>
-        <p>{t(text)}</p>
+    <div id='card' className={className}>
+        <h2>{t(title)}</h2>
+        <div className='hoverfield'>
+            <p>{t(description)}</p>
+            <h3>{t('AboutOsteo.categories.applied')}</h3>
+            <p>{t(text)}</p>
+        </div>
     </div>
-</div>
   )
 }
--- src/sections/AboutOsteoSection/AboutOsteoSection.js
+++ src/sections/AboutOsteoSection/AboutOsteoSection.js
@@ -17,9 +17,9 @@ export default function AboutOsteo() {
                 <div>
                     <p>{t('AboutOsteo.text2')}</p>
                 <div>
-                    <Card img={''} alt={'viszerale-osteopathie-alt'} title={'AboutOsteo.categories.title1'} description={'AboutOsteo.categories.description1'} text={'AboutOsteo.categories.text1'}/>
-                    <Card img={''} alt={'kraniosakrale-osteopathie-alt'} title={'AboutOsteo.categories.title2'} description={'AboutOsteo.categories.description2'} text={'AboutOsteo.categories.text2'}/>
-                    <Card img={''} alt={'parietale-osteopathie-alt'} title={'AboutOsteo.categories.title3'} description={'AboutOsteo.categories.description3'} text={'AboutOsteo.categories.text3'}/>
+                    <Card className={'viszerale-bg'} title={'AboutOsteo.categories.title1'} description={'AboutOsteo.categories.description1'} text={'AboutOsteo.categories.text1'}/>
+                    <Card className={'kraniosakrale-bg'} title={'AboutOsteo.categories.title2'} description={'AboutOsteo.categories.description2'} text={'AboutOsteo.categories.text2'}/>
+                    <Card className={'parietale-bg'}title={'AboutOsteo.categories.title3'} description={'AboutOsteo.categories.description3'} text={'AboutOsteo.categories.text3'}/>
                 </div>
             </div>
         </section>
