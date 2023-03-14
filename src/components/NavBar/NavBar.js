de52b0aaec4d1353c835267230969126002b3cad
Lorelai <nadja.rathenow@rub.de>
2023-03-14 18:56:54 +0100

    NavBar show different when /impressum or /datenschutz, linked logo to home

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -7,18 +7,22 @@ import './styles.scss';
 
 export default function NavBar() {
     const { t } = useTranslation();
+    const myLocation = window.location.pathname
+    console.log(window.location.pathname)
   return (
     <nav className='NavBar'>
         <div className='NavBar_header'>
-            <img src="" alt="logo"/>
+            <a href='/'><img src="" alt="logo"/></a>
             <CTA message={'CTA.Termin'}/>
         </div>
+     {(myLocation !== '/impressum' && myLocation !== '/datenschutz') 
+     &&  
         <div className='NavBar_navigation'>
             <a href='#about-osteo'><h5>{t('NavBar.Osteopathie')}</h5></a>
             <a href='#treatment'><h5>{t('NavBar.Behandlung')}</h5></a>
             <a href='#pricing'><h5>{t('NavBar.Kosten')}</h5></a>
             <a href='#about-me'><h5>{t('NavBar.ÜberMich')}</h5></a>
-        </div>
+        </div>}
     </nav>
   )
 }
